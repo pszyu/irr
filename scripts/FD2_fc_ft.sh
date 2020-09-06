43511b4d85f04d62f132bd2e76da31d052000731
Shuzhi Yu <shuzhiyu@cs.duke.edu>
2020-09-06 16:54:24 -0400

    Past is past. This is a goodbye to the previous experiments.

--- scripts/FD2_fc_ft.sh
+++ scripts/FD2_fc_ft.sh
@@ -2,24 +2,25 @@
 #SBATCH -t 200:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --partition=compsci-gpu
-#SBATCH --constraint=p100
+#SBATCH --constraint=v100
 #SBATCH -o output/slurm-%j.out
 
 # experiments and datasets meta
 EXPERIMENTS_HOME="experiments"
 
 # datasets
-FLYINGCHAIRS_OCC_HOME=/usr/xtmp/shuzhiyu/datasets/FCOcc/FlyingChairsOcc/data
+FLYINGTHINGS_HOME=/usr/xtmp/vision/datasets/FlyingThings3D_subset_filtered/
+SINTEL_HOME=/usr/xtmp/vision/datasets/MPI_Sintel/
 
 # model and checkpoint
 MODEL=IRR_PWC_FED_OEE
 EVAL_LOSS=MultiScaleEPE_PWC_Bi_Occ_upsample
-CHECKPOINT="experiments/IRR_PWC_FED_OEE-20200506-153207/checkpoint_latest.ckpt"
+CHECKPOINT="experiments/IRR_PWC_FED_OEE-2-fc2-20200512-123451/checkpoint_latest.ckpt"
 SIZE_OF_BATCH=3
 
 # save path
 TIME=$(date +"%Y%m%d-%H%M%S")
-SAVE_PATH="$EXPERIMENTS_HOME/$MODEL-2-fc2-$TIME"
+SAVE_PATH="$EXPERIMENTS_HOME/$MODEL-2-fc2-ft1-$TIME"
 
 # training configuration
 python3 ../main.py \
@@ -28,24 +29,25 @@ python3 ../main.py \
 --checkpoint=$CHECKPOINT \
 --lr_scheduler=MultiStepLR \
 --lr_scheduler_gamma=0.5 \
---lr_scheduler_milestones="[54, 72, 90]" \
+--lr_scheduler_milestones="[128, 139, 149]" \
 --model=$MODEL \
 --num_workers=4 \
 --optimizer=Adam \
---optimizer_lr=1e-4 \
+--optimizer_lr=1e-5 \
 --optimizer_weight_decay=4e-4 \
 --save=$SAVE_PATH \
---start_epoch=66 \
---total_epochs=108 \
+--start_epoch=109 \
+--total_epochs=159 \
 --training_augmentation=RandomAffineFlowOcc \
---training_dataset=FlyingChairsOccTrain \
+--training_augmentation_crop="[384,768]" \
+--training_dataset=FlyingThings3dCleanTrain \
 --training_dataset_photometric_augmentations=True \
---training_dataset_root=$FLYINGCHAIRS_OCC_HOME \
+--training_dataset_root=$FLYINGTHINGS_HOME \
 --training_key=total_loss \
 --training_loss=$EVAL_LOSS \
---validation_dataset=FlyingChairsOccValid  \
+--validation_dataset=SintelTrainingCleanFull  \
 --validation_dataset_photometric_augmentations=False \
---validation_dataset_root=$FLYINGCHAIRS_OCC_HOME \
+--validation_dataset_root=$SINTEL_HOME \
 --validation_key=F1 \
 --validation_loss=$EVAL_LOSS \
 --validation_key_minimize=False \
